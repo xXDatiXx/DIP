cb1223a7c9d9df49472e9f5df6b48f7c94db1d9f
Dati <102495187+xXDatiXx@users.noreply.github.com>
2022-06-08 13:24:14 -0500

    Add files via upload
    Falta punto 4

---
+++ task1.m
@@ -0,0 +1,28 @@
+A = imread('mau.jpg');
+
+matriz = rand(5,3);
+matriz(matriz(:,:)>0.52)=1;
+matrizidentidad1 = [1 0 0; 0 1 0; 0 0 1];
+
+
+[M,N]=size(A);
+g=imcrop(A,[1 1 M (N/2)]);
+figure(); imshow(g);
+ 
+
+R=A(:,:,1);
+G=A(:,:,2);
+B=A(:,:,3);
+Comodin = zeros(size(R));
+
+rojo = cat(3,R,Comodin,Comodin);
+figure(); imshow(rojo);
+
+verde = cat(3,Comodin,G,Comodin);
+figure(); imshow(verde)
+
+azul = cat(3,Comodin,Comodin,B);
+figure(); imshow(azul);
+
+gris = cat(3,G,G,G);
+figure(); imshow(gris)
